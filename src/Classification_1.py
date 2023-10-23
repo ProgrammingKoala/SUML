02ef548a54337bc8186e782434972bc2d8b2f2fa
Mark <contact.now.markchen@gmail.com>
2023-10-23 10:43:10 +0200

    add another classification model and change file to classification_1

--- src/Classification_1.py
+++ src/Classification_1.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[1]:
+# In[35]:
 
 
 import time
@@ -12,7 +12,7 @@ import matplotlib.pyplot as plt
 get_ipython().run_line_magic('matplotlib', 'inline')
 
 
-# In[19]:
+# In[36]:
 
 
 path_data = Path().resolve().parent / "testdata" / "sarcasm.json"
@@ -21,7 +21,7 @@ df.head()
 print(df.shape)
 
 
-# In[20]:
+# In[37]:
 
 
 df.drop(df.index[1000:],inplace=True)
@@ -30,20 +30,20 @@ df.head()
 print(df.shape)
 
 
-# In[21]:
+# In[38]:
 
 
 plt.hist(df["is_sarcastic"])
 plt.show()
 
 
-# In[22]:
+# In[39]:
 
 
 from sklearn.feature_extraction.text import CountVectorizer
 
 
-# In[23]:
+# In[40]:
 
 
 cv = CountVectorizer()
@@ -55,25 +55,25 @@ print(df.head())
 print(df.shape)
 
 
-# In[24]:
+# In[41]:
 
 
 pd.isna(df)
 
 
-# In[25]:
+# In[42]:
 
 
 df[df.isna().any(axis=1)]
 
 
-# In[26]:
+# In[43]:
 
 
 df[df.isnull().any(axis=1)]
 
 
-# In[27]:
+# In[44]:
 
 
 dfdelete = np.array(df[df.isna().any(axis=1)].index,df[df.isnull().any(axis=1)].index)
@@ -82,13 +82,13 @@ df.drop(dfdelete,inplace=True)
 print(df.shape)
 
 
-# In[28]:
+# In[45]:
 
 
 df["is_sarcastic"].head()
 
 
-# In[29]:
+# In[46]:
 
 
 from sklearn.model_selection import train_test_split
@@ -97,20 +97,20 @@ X_train, X_test, y_train, y_test = train_test_split(df.drop(["is_sarcastic"],axi
                                                     random_state=111)
 
 
-# In[30]:
+# In[47]:
 
 
 from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import classification_report
 nsimu=200+1
-penalty=[0]*nsimu
+logpenalty=[0]*nsimu
 logmodel=[0]*nsimu
-predictions =[0]*nsimu
-class_report = [0]*nsimu
-f1=[0]*nsimu
+logpredictions =[0]*nsimu
+logclass_report = [0]*nsimu
+logf1=[0]*nsimu
 
 
-# In[31]:
+# In[48]:
 
 
 print("Starting")
@@ -118,15 +118,54 @@ for i in range(1,nsimu):
     logmodel[i] =(LogisticRegression(C=i/1000,solver="liblinear"))
     #logmodel[i] =(LogisticRegression(C=i/nsimu,solver="liblinear"))
     logmodel[i].fit(X_train,y_train)
-    predictions[i] = logmodel[i].predict(X_test)
-    class_report[i] = classification_report(y_test,predictions[i])
-    l=class_report[i].split()
-    f1[i] = l[len(l)-2]
-    penalty[i]=1000/i
+    logpredictions[i] = logmodel[i].predict(X_test)
+    logclass_report[i] = classification_report(y_test,logpredictions[i])
+    l=logclass_report[i].split()
+    logf1[i] = l[len(l)-2]
+    logpenalty[i]=1000/i
 
-plt.scatter(penalty[1:len(penalty)-2],f1[1:len(f1)-2])
-plt.title("F1-score vs. regularization parameter",fontsize=20)
+plt.scatter(logpenalty[1:len(logpenalty)-2],logf1[1:len(logf1)-2])
+plt.title("Logistic Regression: F1-score vs. regularization parameter",fontsize=20)
 plt.xlabel("Penalty parameter",fontsize=17)
 plt.ylabel("F1-score on test data",fontsize=17)
 plt.show()
+
+
+# In[52]:
+
+
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.metrics import classification_report
+nsimu=200+1
+maxdepth=[0]*nsimu
+dectree=[0]*nsimu
+decpredictions =[0]*nsimu
+decclass_report = [0]*nsimu
+decf1=[0]*nsimu
+
+
+# In[ ]:
+
+
+print("Starting")
+for i in range(1,nsimu):
+    dectree[i] =DecisionTreeClassifier(max_depth=pow(i,2))
+    dectree[i].fit(X_train,y_train)
+    decpredictions[i] = dectree[i].predict(X_test)
+    decclass_report[i] = classification_report(y_test,decpredictions[i])
+    l=decclass_report[i].split()
+    decf1[i] = l[len(l)-2]
+    maxdepth[i]=pow(i,2)
+
+plt.scatter(maxdepth[1:len(maxdepth)-2],decf1[1:len(decf1)-2])
+plt.title("Decision Tree: F1-score vs. regularization parameter",fontsize=20)
+plt.xlabel("Max-depth parameter",fontsize=17)
+plt.ylabel("F1-score on test data",fontsize=17)
+plt.show()
+
+
+# In[ ]:
+
+
+
 
---
+++ src/sample/pythonProject/readWrite.py
@@ -0,0 +1,6 @@
+class ReadWrite:
+    def write_line(self, text: str):
+        print(text)
+
+    def read_line_as_int(self, value):
+        return int(input(value))
